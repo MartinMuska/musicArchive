ac7d78b89deb8d9becd418b61e67efbe32f35776
martinMuska <muskaonline@seznam.cz>
2025-11-03 18:41:59 +0100

    add bands

---
+++ database/albums.js
@@ -0,0 +1,24 @@
+const albums = [
+    { id: "a1", bandName: "The Beatles", title: "Abbey Road", year: 1969, rating: 4},
+    { id: "a2", bandName: "The doors", title: "The Doors", year: 1967, rating: 5},
+    { id: "a3", bandName: "Gentle Giant", title: "Gentle Giant", year: 1970, rating: 5},
+    { id: "a4", bandName: "Gentle Giant", title: "Free Hand", year: 1975, rating: 4},
+    { id: "a5", bandName: "Dream Theater", title: "Awake", year: 1994, rating: 5},
+    { id: "a6", bandName: "Death", title: "Symbolic", year: 1995, rating: 5},
+    { id: "a7", bandName: "Death", title: "The Sound Of Perseverance", year: 1998, rating: 5},
+    { id: "a8", bandName: "Voivod", title: "Dimension Hatröss", year: 1988, rating: 5},
+    { id: "a9", bandName: "Voivod", title: "Nothingface", year: 1990, rating: 5},
+    { id: "a10",bandName: "Voivod", title: "The Wake", year: 2018, rating: 5},
+    { id: "a12",bandName: "Overkill", title: "The Years Of Decay", year: 1989, rating: 5},
+    { id: "a13",bandName: "Cytotoxin", title: "Gamagedon", year: 2017, rating: 5},
+    { id: "a14",bandName: "Pink Floyd", title: "Animals", year: 1977, rating: 4},
+    { id: "a15",bandName: "Iron Maiden", title: "Powerslave", year: 1984, rating: 5},
+    { id: "a16",bandName: "Judas Priest", title: "Painkiller", year: 1990, rating: 5},
+    { id: "a17",bandName: "Black Sabbath", title: "Master Of Reality", year: 1971, rating: 5},
+    { id: "a18",bandName: "Arch Enemy", title: "Will To Power", year: 2017, rating: 3},
+    { id: "a19",bandName: "Motorhead", title: "Ace Of Spades", year: 1980, rating: 4},
+    { id: "a20",bandName: "AC/DC", title: "T.N.T.", year: 1970, rating: 4},
+    { id: "a21",bandName: "Rhapsody", title: "Symphony Of Enchanted Lands", year: 1998, rating: 5},
+];
+
+
